947dd0ca97bb41eb67a50810f854964b95d9ef53
cantonbolo <cantonbolo@gmail.com>
2019-11-21 21:07:53 +0800

    init

--- src/GeneratorsServiceProvider.php
+++ src/GeneratorsServiceProvider.php
@@ -2,29 +2,29 @@
 
 namespace Xylophone\Generators;
 
-use Xylophone\Generators\Console\Commands\ConfigBackpackCommand;
-use Xylophone\Generators\Console\Commands\CrudBackpackCommand;
-use Xylophone\Generators\Console\Commands\CrudControllerBackpackCommand;
-use Xylophone\Generators\Console\Commands\CrudModelBackpackCommand;
-use Xylophone\Generators\Console\Commands\CrudOperationBackpackCommand;
-use Xylophone\Generators\Console\Commands\CrudRequestBackpackCommand;
-use Xylophone\Generators\Console\Commands\ModelBackpackCommand;
-use Xylophone\Generators\Console\Commands\RequestBackpackCommand;
-use Xylophone\Generators\Console\Commands\ViewBackpackCommand;
+use Xylophone\Generators\Console\Commands\ConfigXylophoneCommand;
+use Xylophone\Generators\Console\Commands\CrudXylophoneCommand;
+use Xylophone\Generators\Console\Commands\CrudControllerXylophoneCommand;
+use Xylophone\Generators\Console\Commands\CrudModelXylophoneCommand;
+use Xylophone\Generators\Console\Commands\CrudOperationXylophoneCommand;
+use Xylophone\Generators\Console\Commands\CrudRequestXylophoneCommand;
+use Xylophone\Generators\Console\Commands\ModelXylophoneCommand;
+use Xylophone\Generators\Console\Commands\RequestXylophoneCommand;
+use Xylophone\Generators\Console\Commands\ViewXylophoneCommand;
 use Illuminate\Support\ServiceProvider;
 
 class GeneratorsServiceProvider extends ServiceProvider
 {
     protected $commands = [
-        ConfigBackpackCommand::class,
-        CrudModelBackpackCommand::class,
-        CrudControllerBackpackCommand::class,
-        CrudOperationBackpackCommand::class,
-        CrudRequestBackpackCommand::class,
-        CrudBackpackCommand::class,
-        ModelBackpackCommand::class,
-        RequestBackpackCommand::class,
-        ViewBackpackCommand::class,
+        ConfigXylophoneCommand::class,
+        CrudModelXylophoneCommand::class,
+        CrudControllerXylophoneCommand::class,
+        CrudOperationXylophoneCommand::class,
+        CrudRequestXylophoneCommand::class,
+        CrudXylophoneCommand::class,
+        ModelXylophoneCommand::class,
+        RequestXylophoneCommand::class,
+        ViewXylophoneCommand::class,
     ];
 
     /**
